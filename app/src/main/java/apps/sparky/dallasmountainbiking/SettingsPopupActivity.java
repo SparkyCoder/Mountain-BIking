2b26fb0cd395e7df1d69f0ffec24f8d677635ab1
unknown <David.Kobuszewski@PLNA004300-W530.hps.com>
2016-01-31 01:47:52 -0600

    One of the math equations was missing a number causing medium interval to be selected every time. This was fixed in this commit. Also added some generic exception handling.

--- app/src/main/java/apps/sparky/dallasmountainbiking/SettingsPopupActivity.java
+++ app/src/main/java/apps/sparky/dallasmountainbiking/SettingsPopupActivity.java
@@ -5,15 +5,19 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import apps.sparky.dallasmountainbiking.BLL.Exceptions.ExceptionHandling;
 import apps.sparky.dallasmountainbiking.BLL.Services.BackgroundService;
 import apps.sparky.dallasmountainbiking.BLL.Services.RepeatingIntent;
 import apps.sparky.dallasmountainbiking.DAL.DAO;
 import apps.sparky.dallasmountainbiking.Objects.IntervalSetting;
 
 public class SettingsPopupActivity extends Activity {
-private DAO dao;
+    private DAO dao;
+private ExceptionHandling exceptionHandling;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +27,9 @@ private DAO dao;
         SetupClicks();
     }
 
-    private void Initialize(){
+    private void Initialize() {
         dao = new DAO();
+        exceptionHandling = new ExceptionHandling(this);
         SetupDefaultSelectedButton();
     }
 
@@ -35,53 +40,62 @@ private DAO dao;
 
         if (currentSetting == null) {
             settings.check(R.id.mediumInterval);
-        } else if (currentSetting.intervalValue == 100 * 60 * 60 * 10) {
+        }
+        if (currentSetting.intervalValue == 1000 * 60 * 60 * 10) {
             settings.check(R.id.lowInterval);
-        } else if (currentSetting.intervalValue == 1000 * 60 * 60 * 1) {
+        }
+        if (currentSetting.intervalValue == 1000 * 60 * 60 * 1) {
             settings.check(R.id.highInterval);
+        }
+        if (currentSetting.intervalValue == 1000 * 60 * 60 * 5) {
+            settings.check(R.id.mediumInterval);
         }
     }
 
     private void SetupClicks() {
-        RadioGroup settings = (RadioGroup) findViewById(R.id.settingsGroup);
+        final RadioGroup settings = (RadioGroup) findViewById(R.id.settingsGroup);
 
         settings.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                IntervalSetting currentSetting = dao.GetIntervalSettings();
+                try {
+                    IntervalSetting currentSetting = dao.GetIntervalSettings();
 
-                long value = (1000 * 60 * 60 * 5);
+                    long value = 1000 * 60 * 60 * 5;
 
-                if (checkedId == R.id.lowInterval) {
-                    value = (100 * 60 * 60 * 10);
-                }
-                else if (checkedId == R.id.mediumInterval)
-                {
-                    value = (1000 * 60 * 60 * 5);
-                }
-                else if (checkedId == R.id.highInterval){
-                    value = (1000 * 60 * 60 * 1);
-                }
+                    RadioButton radiobutton = (RadioButton) findViewById(checkedId);
+                    if (checkedId == R.id.highInterval)
+                        value = 1000 * 60 * 60 * 1;
+                    if (checkedId == R.id.mediumInterval)
+                        value = 1000 * 60 * 60 * 5;
+                    if (checkedId == R.id.lowInterval)
+                        value = 1000 * 60 * 60 * 10;
 
-                if(currentSetting == null){
-                    IntervalSetting newSetting = new IntervalSetting("BackgroundService", value);
-                    newSetting.save();
-                }
-                else{
-                    currentSetting.intervalValue = value;
-                    currentSetting.save();
-                }
 
-                CancelService();
-                startService(new Intent(getApplicationContext(), BackgroundService.class));
+                    if (currentSetting == null) {
+                        IntervalSetting newSetting = new IntervalSetting("BackgroundService", value);
+                        newSetting.save();
+                    } else {
+                        currentSetting.intervalValue = value;
+                        currentSetting.save();
+                    }
+
+                    CancelService();
+                    startService(new Intent(getApplicationContext(), BackgroundService.class));
+                } catch (Exception ex) {
+                    exceptionHandling.ShowToast(ex.getMessage());
+                }
             }
         });
     }
 
-    private void CancelService(){
-        PendingIntent.getBroadcast(this, 0,
+    private void CancelService() {
+        PendingIntent intent = PendingIntent.getBroadcast(this, 0,
                 new Intent(this, RepeatingIntent.class),
-                PendingIntent.FLAG_NO_CREATE).cancel();
+                PendingIntent.FLAG_NO_CREATE);
+
+        if (intent != null)
+            intent.cancel();
     }
 }
